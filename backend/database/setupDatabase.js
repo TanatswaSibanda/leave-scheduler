b3b40aa33284473cbff61e0e2d4fd448ddd685c1
TanatswaSibanda <tanatswa.m.sibanda@gmail.com>
2026-07-01 14:11:57 +0200

    Create employees table in SQLite database

--- backend/database/setupDatabase.js
+++ backend/database/setupDatabase.js
@@ -2,8 +2,15 @@ const connectDB = require("./database");
 
 async function setupDatabase() {
     const db = await connectDB();
+    await db.exec(`
+    CREATE TABLE IF NOT EXISTS employees (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        team TEXT NOT NULL
+    )
+`);
 
-    console.log("Connected to database");
+    console.log("Employees table created");
 }
 
 setupDatabase();
